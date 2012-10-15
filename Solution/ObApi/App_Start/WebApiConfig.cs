20422037b2a838cd839d1ec95d093a2092eab671
John Booty <johnedmundrose@gmail.com>
2012-10-15 17:19:27 -0400

    Json is default now, not XML

---
+++ Solution/ObApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ObApi {
+	public static class WebApiConfig {
+
+		// via: http://stackoverflow.com/questions/9847564/how-do-i-get-mvc-4-webapi-to-return-json-instead-of-xml-using-chrome
+		public static void Register(HttpConfiguration config) {
+			config.Routes.MapHttpRoute(
+				 name: "DefaultApi",
+				 routeTemplate: "api/{controller}/{id}",
+				 defaults: new {
+					 id = RouteParameter.Optional
+				 }
+			);
+
+			var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+			config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+		}
+	}
+
+	/*
+	public static void Register(HttpConfiguration config) {
+		config.Routes.MapHttpRoute(
+			 name: "DefaultApi",
+			 routeTemplate: "api/{controller}/{id}",
+			 defaults: new {
+				 id = RouteParameter.Optional
+			 }
+		);
+	}
+	 * */
+
+}
