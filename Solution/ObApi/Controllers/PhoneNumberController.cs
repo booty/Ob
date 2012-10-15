0215f0ff661c53fd51bef3fafc7d14a8974d7bb4
John Booty <johnedmundrose@gmail.com>
2012-10-15 17:20:50 -0400

    Updated all Nuget packages to latest

---
+++ Solution/ObApi/Controllers/PhoneNumberController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using ObApi.Models;
+
+
+
+namespace ObApi.Controllers {
+	public class PhoneNumberController : ApiController {
+		// GET api/phonenumber
+		
+		
+		public IEnumerable<PhoneNumber> Get(string token, bool friendsOnly=false) {
+			return PhoneNumber.Find(1238, friendsOnly);
+		}
+
+
+		// GET api/phonenumber/5
+		public string Get(int id) {
+			return "value";
+		}
+
+		// POST api/phonenumber
+		public void Post([FromBody]string value) {
+
+		}
+
+		// PUT api/phonenumber/5
+		public void Put(int id, [FromBody]string value) {
+
+		}
+
+		// DELETE api/phonenumber/5
+		public void Delete(int id) {
+			
+		}
+
+	}
+}
---
+++ Solution/ObCore/Models/PhoneNumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using ObCore;
+using ObCore.Helpers;
+using ObCore.Models;
+
+namespace ObApi.Models {
+	public class PhoneNumber {
+		public int IdMember;
+		public int? IdPictureMember;
+		public string Login, PhoneNumberUs, LoginsPrevious, FirstName, LastName, URL;
+		public DateTime ClitterPreferencesUpdated;
+
+		public static List<PhoneNumber> Find(int idMember, bool friendsOnly = true) {
+			string sql = String.Format("select * from ClitterPhoneNumbers({0},{1}) order by login", idMember, Convert.ToInt32(friendsOnly));
+			var da = new DataAccess();
+			List<PhoneNumber> results;
+			using (var dt = da.GetDataTable(sql)) {
+				results = new List<PhoneNumber>(dt.Rows.Count);
+				foreach (DataRow dr in dt.Rows) {
+					results.Add(ToPhoneNumber(dr));
+				}
+			}
+			return results;
+		}
+
+		private static PhoneNumber ToPhoneNumber(DataRow dr) {
+			var pn = new PhoneNumber {
+				ClitterPreferencesUpdated = (DateTime) dr["clitter_preferences_updated"],
+				FirstName = (String) dr["first_name"],
+				LastName = (String) dr["last_name"],
+				Login = (String) dr["login"],
+				//LoginsPrevious = (String) dr["logins_previous"],
+				IdMember = (int) dr["id_member"],
+				PhoneNumberUs = (string) dr["phone_number_us"]
+			};
+			pn.URL = Picture.PublicPictureUrl(pn.IdMember, PictureSize.Small50Px);
+			if (dr["logins_previous"] != DBNull.Value) pn.LoginsPrevious = (string) dr["logins_previous"]; 
+			if (dr["id_picture_member"] != DBNull.Value) pn.IdPictureMember = (int) dr["id_picture_member"]; 
+
+
+			return pn;
+
+		}
+
+	}
+}
