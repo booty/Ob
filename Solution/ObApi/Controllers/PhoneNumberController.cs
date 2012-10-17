d55c6bcf64b3d81e24df76c7518dd68eb469dc2f
John Booty <johnedmundrose@gmail.com>
2012-10-17 16:01:32 -0400

    Removed unused methods, add inline doc

--- Solution/ObApi/Controllers/PhoneNumberController.cs
+++ Solution/ObApi/Controllers/PhoneNumberController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using Newtonsoft.Json.Linq;
@@ -13,32 +14,22 @@ using ObApi.Models;
 namespace ObApi.Controllers {
 	public class PhoneNumberController : ApiController {
 		// GET api/phonenumber
-		
-		
-		public IEnumerable<PhoneNumber> Get(string token, bool friendsOnly=false) {
-			return PhoneNumber.Find(1238, friendsOnly);
+		/// <summary>
+		/// Get a list of phone numbers visible to a single user
+		/// </summary>
+		/// <param name="token">Authentication token</param>
+		/// <param name="friendsOnly">If true, only show phone numbers for members this member has friended. 
+		/// If false, show all phone numbers that others have shared with this member, even if this member
+		/// hasn't friended them.</param>
+		/// <returns></returns>
+		public HttpResponseMessage Get(string token, bool friendsOnly = false) {
+			var response = Request.CreateResponse<IEnumerable<PhoneNumber>>(HttpStatusCode.OK, PhoneNumber.Find(1238, friendsOnly));
+			//response.Headers.Add("Your-Mom", "Is A Huge Fucking Slut");
+			response.Headers.Add("Cache-Control", "private");
+			response.Content.Headers.Add("Expires", DateTime.UtcNow.AddMinutes(60).ToString("R"));
+			return response;
 		}
 
-
-		// GET api/phonenumber/5
-		public string Get(int id) {
-			return "value";
-		}
-
-		// POST api/phonenumber
-		public void Post([FromBody]string value) {
-
-		}
-
-		// PUT api/phonenumber/5
-		public void Put(int id, [FromBody]string value) {
-
-		}
-
-		// DELETE api/phonenumber/5
-		public void Delete(int id) {
-			
-		}
 
 	}
 }
