c84cf31a35eaa5936d59617e673750444e1e11b1
John Booty <johnedmundrose@gmail.com>
2012-10-17 16:03:01 -0400

    Whoops!

--- Solution/ObApi/Controllers/PhoneNumberController.cs
+++ Solution/ObApi/Controllers/PhoneNumberController.cs
@@ -24,8 +24,7 @@ namespace ObApi.Controllers {
 		/// <returns></returns>
 		public HttpResponseMessage Get(string token, bool friendsOnly = false) {
 			var response = Request.CreateResponse<IEnumerable<PhoneNumber>>(HttpStatusCode.OK, PhoneNumber.Find(1238, friendsOnly));
-			//response.Headers.Add("Your-Mom", "Is A Huge Fucking Slut");
-			response.Headers.Add("Cache-Control", "private");
+			response.Content.Headers.Add("Cache-Control", "private");
 			response.Content.Headers.Add("Expires", DateTime.UtcNow.AddMinutes(60).ToString("R"));
 			return response;
 		}
