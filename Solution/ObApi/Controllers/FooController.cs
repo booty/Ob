a3050ac842977b19e4cc65897aa2476979ccdb0a
John Booty <johnedmundrose@gmail.com>
2012-10-20 12:21:27 -0400

    First working implementation of auth controller

---
+++ Solution/ObApi/Controllers/FooController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace ObApi.Controllers {
+	public class FooController : ApiController {
+		// GET api/foo
+		public string Get() {
+			int? memberId=HttpContext.Current.MemberId();
+			return memberId.ToString();
+		}
+
+		// GET api/foo/5
+		public string Get(int id) {
+			return "value";
+		}
+
+		// POST api/foo
+		public void Post([FromBody]string value) {
+		}
+
+		// PUT api/foo/5
+		public void Put(int id, [FromBody]string value) {
+		}
+
+		// DELETE api/foo/5
+		public void Delete(int id) {
+		}
+	}
+}
