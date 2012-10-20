7fced119572a52ff1d8e023959445d2a76ff6028
John Booty <johnedmundrose@gmail.com>
2012-10-20 12:20:09 -0400

    Removed authtoken from params

--- Solution/ObApi/Controllers/PhoneNumberController.cs
+++ Solution/ObApi/Controllers/PhoneNumberController.cs
@@ -17,16 +17,12 @@ namespace ObApi.Controllers {
 		/// <summary>
 		/// Get a list of phone numbers visible to a single user
 		/// </summary>
-		/// <param name="token">Authentication token</param>
 		/// <param name="friendsOnly">If true, only show phone numbers for members this member has friended. 
 		/// If false, show all phone numbers that others have shared with this member, even if this member
 		/// hasn't friended them.</param>
 		/// <returns></returns>
-		public HttpResponseMessage Get(string token, bool friendsOnly = false) {
-			var response = Request.CreateResponse<IEnumerable<PhoneNumber>>(HttpStatusCode.OK, PhoneNumber.Find(1238, friendsOnly));
-			response.Headers.Add("Your-Mom", "Is A Huge Fucking Slut");
-			response.Headers.Add("Cache-Control", "private");
-			response.Content.Headers.Add("Expires", DateTime.UtcNow.AddMinutes(60).ToString("R"));
+		public HttpResponseMessage Get(bool friendsOnly = false) {
+			var response = Request.CreateResponse<IEnumerable<PhoneNumber>>(HttpStatusCode.OK, PhoneNumber.Find(1238, friendsOnly)).WithObApiDefaults();
 			return response;
 		}
 
