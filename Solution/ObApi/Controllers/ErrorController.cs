ebf2c8bdc1c7942fbdbc1fd03b3d029c0ff9e4e3
John Booty <johnedmundrose@gmail.com>
2012-10-22 17:09:08 -0400

    For testing; ignore it

---
+++ Solution/ObApi/Controllers/ErrorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ObApi.Controllers {
+	public class ErrorController : ApiController {
+		// GET api/error
+		public string Get() {
+			return "Penis";
+		}
+
+		// GET api/error/5
+		public string Get(int id) {
+			return "value";
+		}
+
+		// POST api/error
+		public void Post([FromBody]string value) {
+		}
+
+		// PUT api/error/5
+		public void Put(int id, [FromBody]string value) {
+		}
+
+		// DELETE api/error/5
+		public void Delete(int id) {
+		}
+	}
+}
