18e5ddcd705098cb0396a6a5a78b6b9038fb7efa
John Booty <johnedmundrose@gmail.com>
2012-10-22 17:09:44 -0400

    Renamed from PhoneNumberController

---
+++ Solution/ObApi/Controllers/PhoneNumbersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using ObApi.Models;
+
+
+
+namespace ObApi.Controllers {
+	public class PhoneNumbersController : ApiController {
+		// GET api/phonenumber
+		/// <summary>
+		/// Get a list of phone numbers visible to a single user
+		/// </summary>
+		/// <param name="friendsOnly">If true, only show phone numbers for members this member has friended. 
+		/// If false, show all phone numbers that others have shared with this member, even if this member
+		/// hasn't friended them.</param>
+		/// <returns></returns>
+		public HttpResponseMessage Get(bool friendsOnly = false) {
+			int? memberId = HttpContext.Current.MemberId();
+			if (!memberId.HasValue) return Request.CreateMissingAuthorizationTokenResponse();
+			return Request.CreateResponse<IEnumerable<PhoneNumber>>(HttpStatusCode.OK, PhoneNumber.Find(memberId.Value, friendsOnly)).WithObApiDefaults();
+		}
+
+
+	}
+}
