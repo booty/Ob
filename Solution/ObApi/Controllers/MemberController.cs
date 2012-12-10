58b14110cf2ac60625332ec3d6c76d9a4ac4cac3
John Booty <johnedmundrose@gmail.com>
2012-12-09 22:32:51 -0500

    Added Member controller to retrieve member profiles

---
+++ Solution/ObApi/Controllers/MemberController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ObCore.Models;
+
+namespace ObApi.Controllers {
+	public class MemberController : ApiController {
+		// /api/member/JohnBooty
+		public HttpResponseMessage Get(string id) {
+			int memberId;
+			Member m;
+			// todo: I guess somebody is fucked if their login is also a valid integer! oh well!
+			m = Int32.TryParse(id, out memberId) ? Member.Find(memberId) : Member.Find(id);
+			if (m == null) return Request.CreateResponse<string>(HttpStatusCode.NotFound, "No member with that past or present login.");
+			return Request.CreateResponse(HttpStatusCode.OK, m).WithObApiDefaults();
+		}
+
+	}
+}
