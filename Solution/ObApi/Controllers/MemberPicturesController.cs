77615b69711fa9b3990bb6d43f957b869654e83d
booty <johnedmundrose@gmail.com>
2013-02-27 21:39:31 -0500

    Moved "else" block to reduce nesting

--- Solution/ObApi/Controllers/MemberPicturesController.cs
+++ Solution/ObApi/Controllers/MemberPicturesController.cs
@@ -9,6 +9,9 @@ using ObCore;
 using ObCore.Models;
 
 namespace ObApi.Controllers {
+	/// <summary>
+	/// Faggot sauce!
+	/// </summary>
 	public class MemberPicturesController : ApiController {
 		// GET api/memberpictures
 		//public HttpResponseMessage Get(int? skip=0, int? take=25) {
@@ -19,26 +22,39 @@ namespace ObApi.Controllers {
 		 * */
 
 		// GET api/memberpictures/5
+		/// <summary>
+		/// One or more member pictures.
+		/// </summary>
+		/// <param name="id">Optional. Use this if you're just requesting a single picture. Either the picture's numeric ID (for public pictures) or the GUID (for Friends-Only pictures).</param>
+		/// <param name="skip">How many pictures to skip? Use this for paging, etc. If you want to start at the 51st picture, then skip=50. Ignored if you specify "id"</param>
+		/// <param name="take">How many pictures to return? Use this for paging, etc. Ignored if you specify "id". May be capped.</param>
+		/// <returns></returns>
 		public HttpResponseMessage Get(string id="", int? skip=0, int? take=25) {
+
 			int idPictureMember;
 			MemberPicture mp;
+			
+			// Public pic (id is a valid int)
 			if (Int32.TryParse(id, out idPictureMember)) {
 				mp= MemberPicture.FindPublicPicture(idPictureMember, true);
 				if (mp == null) return Request.CreateResponse(HttpStatusCode.NotFound, "That profile picture doesn't exist, or can't be viewed.").WithObApiPublicDefaults();
 				return Request.CreateResponse<MemberPicture>(HttpStatusCode.OK, mp).WithObApiPublicDefaults();
 			}
-			else {
-				if (id == "") {
-					return Request.CreateResponse(HttpStatusCode.OK, MemberPicture.FindPublicPictures(skip.Value,take.Value,true)).WithObApiPublicDefaults();
-				}
-				else {
-					int? memberId = HttpContext.Current.MemberId();
-					if (!memberId.HasValue) return Request.CreateMissingAuthorizationTokenResponse();
-					var member = Member.Find(memberId.Value);
-					if (member == null) return Request.CreateMissingAuthorizationTokenResponse();
-					return Request.CreateResponse(HttpStatusCode.OK, member.GetFriendsOnlyPicture(id)).WithObApiPrivateDefaults();
-				}
+			
+			
+			// Range of pics (default: take from all public pics, newest first)
+			if (id == "") {
+				if (take > 100) take = 100;  // todo: move this magic number to web.config
+				return Request.CreateResponse(HttpStatusCode.OK, MemberPicture.FindPublicPictures(skip.Value,take.Value,true)).WithObApiPublicDefaults();
 			}
+			
+			// Fop (id is something else; presumably a string). Auth is required.
+			int? memberId = HttpContext.Current.MemberId();
+			if (!memberId.HasValue) return Request.CreateMissingAuthorizationTokenResponse();
+			var member = Member.Find(memberId.Value);
+			if (member == null) return Request.CreateMissingAuthorizationTokenResponse();
+			return Request.CreateResponse(HttpStatusCode.OK, member.GetFriendsOnlyPicture(id)).WithObApiPrivateDefaults();
+			
 		}
 
 		/*
