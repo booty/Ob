faaafbd26d76d500544dc05df808b1e4d73be421
booty <johnedmundrose@gmail.com>
2013-03-27 16:58:35 -0400

    Progress! Maybe.

---
+++ Solution/ObApiNancy/Modules/DocumentationModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Routing;
+
+namespace ObApiNancy.Modules {
+	public class DocumentationModule : NancyModule {
+		public DocumentationModule(IRouteCacheProvider routeCacheProvider)
+			: base("/docs") {
+			Get["/"] = p => {
+				var foo = routeCacheProvider.GetCache();
+
+				return View["routes.cshtml", routeCacheProvider.GetCache()];
+
+			};
+		}
+	}
+}
